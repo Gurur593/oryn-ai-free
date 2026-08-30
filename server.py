74a4331abb719d043bebae297284627e7fb5925b
Gurur593 <bjk064128@gmail.com>
2026-08-30 18:43:29 +0300

    server.py

--- server.py
+++ server.py
@@ -95,4 +95,6 @@ def saglik():
     return jsonify({'status': 'ORYN ÜCRETSİZ çalışıyor! 🚀'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
